68d88f47ea7bf670fe815a946774d2312c23bf32
Tiago Gomes Pessoa de Moraes <tgpmoraes@gmail.com>
2025-02-12 17:59:06 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/veraxion/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.veraxion.workshopmongo.resources.exception;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.veraxion.workshopmongo.services.exception.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		String error = "Object not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
--- src/main/java/com/veraxion/workshopmongo/services/UserService.java
+++ src/main/java/com/veraxion/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.veraxion.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.veraxion.workshopmongo.domain.User;
 import com.veraxion.workshopmongo.repository.UserRepository;
+import com.veraxion.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
